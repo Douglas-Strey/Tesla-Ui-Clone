94acc8cdc420386918fa2b91954d53dd78a253a4
Douglas Strey <douglas.morini@hotmail.com>
2021-09-25 10:41:39 -0300

    Criado o context dos veículos e exportado o mesmo

---
+++ src/components/Model/ModelsContext.ts
@@ -0,0 +1,17 @@
+import React, { ReactNode } from 'react';
+
+interface CarModel {
+    modelName: string;
+    overlayNode: ReactNode; // serve para linkar o overlay a própria section
+    sectionRef: React.RefObject<HTMLElement>;
+};
+
+interface ModelsContext {
+    wrapperRef: React.RefObject<HTMLElement>;
+    registeredModels: CarModel[];
+    registerModel: (model: CarModel) => void;
+    unregisterModel: (modelName: string) => void;
+    getModelByName: (modelName: string) => CarModel | null;
+};
+
+export default React.createContext<ModelsContext>({} as ModelsContext)
